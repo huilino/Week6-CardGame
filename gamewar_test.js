ab36de582746464b3e8232189cc41c3160f64324
Hui Lin (Lynn) Olson <f_huilino@marvin.com>
2022-03-11 22:31:16 -0600

    Made changes to test

--- gamewar_test.js
+++ gamewar_test.js
@@ -4,22 +4,27 @@ describe('GameWar', function() {
     describe('#createDeck', function(){
         it('should return a deck of 52 cards with 4 suits and 13 ranks', function(){
             
-            let deck1 = new GameTable;
+            let deck1 = new GameTable();
             deck1.createDeck();
-  
+
             expect(deck1.deck.length).to.equal(52);
             expect(deck1.suits.length).to.equal(4);
             expect(deck1.ranks.length).to.equal(13);
-            expect(deck1.deck[0][1]).to.equal("Clubs");
-            expect(deck1.deck[3][0]).to.equal("4");
+                   
 
         });
 
-        it('should throw an error if the deck does not have 52 cards ', function(){
+        it('should throw an error if the deck does not have all the suits or ranks ', function(){
         
-                let deck1 = new GameTable;  
+                let deck1 = new GameTable();  
                 deck1.createDeck();
-                expect(deck1.deck.length).to.equal(52);
+
+                
+                expect(deck1.suits).to.include("Clubs");
+                expect(deck1.ranks).to.include("K");
+                expect(deck1.deck[0][1]).to.equal("Clubs");
+                expect(deck1.deck[3][0]).to.equal("4");
+                
              
         }).to.throw(Error);
     });
